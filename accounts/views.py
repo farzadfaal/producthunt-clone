fbba7104b739418e795c0259e28d0b6704971e9f
Farzad <farzadfaal@outlook.com>
2020-12-15 19:29:23 +0330

    doing some user stuff...

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
-# Create your views here.
+
+def accounts(request):
+    return render(request, 'accounts/index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        # The user wants to sign up!
+        if request.POST['password'] == request.POST['password_confirmation']:
+            user = User.objects.get(username=request.POST['username'])
+            return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
+    else:
+        # The User wants to enter info
+        return render(request, 'accounts/signup.html')
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    # todo need to logout to home page and logout
+    return render(request, 'accounts/signup.html')
