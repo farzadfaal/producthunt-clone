debe984aa1c288c18729c930b7fef5611aae49dc
Farzad <farzadfaal@outlook.com>
2020-12-15 20:36:52 +0330

    doing some user stuff...

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
@@ -11,17 +11,35 @@ def signup(request):
     if request.method == 'POST':
         # The user wants to sign up!
         if request.POST['password'] == request.POST['password_confirmation']:
-            user = User.objects.get(username=request.POST['username'])
-            return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error': 'Username has already been taken'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error': 'Passwords Doesn\'t match'})
     else:
         # The User wants to enter info
         return render(request, 'accounts/signup.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'Username and/or password is incorrect.'})
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
     # todo need to logout to home page and logout
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
     return render(request, 'accounts/signup.html')
